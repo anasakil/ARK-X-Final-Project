981b1c6fc527cc1fd0737631a367b577cc4b087f
fati <fati.email@gmail.com>
2024-03-04 16:28:40 +0100

    category controller

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,23 @@
+// routes/categoryRoutes.js
+const express = require('express');
+const {
+  createCategory,
+  getAllCategories,
+  getCategoryById,
+  updateCategory,
+  deleteCategory
+} = require('../controllers/categoryController');
+const { protect, restrictTo } = require('../middlewares/authMiddleware');
+
+const router = express.Router();
+
+router.route('/')
+  .get(getAllCategories)
+  .post(protect, restrictTo('admin'), createCategory);
+
+router.route('/:id')
+  .get(getCategoryById)
+  .patch(protect, restrictTo('admin'), updateCategory)
+  .delete(protect, restrictTo('admin'), deleteCategory);
+
+module.exports = router;
