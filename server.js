613b5862fb7451cbbeec7a7bed9c6dc6fb5bf484
badr <dridakhbadr@gmail.com>
2024-03-10 20:49:36 +0100

    creation of new file config/db.js and update server.js

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db.js');
 
 
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
 
+connectDB();
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-mongoose.connect(process.env.MONGO_URI, {
-
-}).then(() => console.log('DB Connected'));
 
 // Routes
 app.use('/api/users', userRoutes);
